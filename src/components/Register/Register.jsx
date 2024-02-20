50bde916534cf77a5c2ec34772bbda1269278980
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-20 21:48:33 +0600

    register from created

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Register.css';
 
 const Register = () => {
      return (
           <div>
-               <h2>This is Register</h2>
+               <h2>Register here</h2>
+               <form>
+                    <input type="email" name="email" id="email" placeholder='Your email' />
+                    <br />
+                    <input type="password" name="password" id="password" placeholder='Your password' />
+                    <br />
+                    <input type="submit" value="Register" />
+               </form>
           </div>
      );
 };
