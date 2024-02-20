e95d28f4f7a415caeceb2e22982f39c2d1f6ce19
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-20 22:15:31 +0600

    get email, pass and submit preventDefault

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 
 const Register = () => {
+
+     const [email, setEmail] = useState('');
+     const [password, setPassword] = useState('');
+
+     const handleSubmit = (event) => {
+          event.preventDefault();
+          const email = event.target.email.value;
+          const password = event.target.password.value;
+          console.log(email, password);
+     };
+
+     const handlePasswordBlur = (event) => {
+          // console.log(event.target.value);
+          setEmail(event.target.value);
+     };
+
+     const handleEmailChange = (event) => {
+          // console.log(event.target.value);
+          setPassword(event.target.value);
+     };
      return (
           <div>
                <h2>Register here</h2>
-               <form>
-                    <input type="email" name="email" id="email" placeholder='Your email' />
+               <form onSubmit={handleSubmit}>
+                    <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your email' />
                     <br />
-                    <input type="password" name="password" id="password" placeholder='Your password' />
+                    <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your password' />
                     <br />
                     <input type="submit" value="Register" />
                </form>
