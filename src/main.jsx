37e5df50b0b0d2865b827cf8d5b9b9a5f8acd096
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-21 00:05:26 +0600

    import form from bootstap

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Main from './components/layout/Main.jsx'
